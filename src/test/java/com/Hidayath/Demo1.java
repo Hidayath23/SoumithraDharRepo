0250f16494c32f1590e8dbf42d90f7ae7f062c1d
Hidayath23 <shaikehidayath64@gmail.com>
2023-09-30 15:49:17 +0530

    Demo First

---
+++ src/test/java/com/Hidayath/Demo1.java
@@ -0,0 +1,12 @@
+package com.Hidayath;
+
+import org.testng.annotations.Test;
+
+public class Demo1 
+{
+	@Test
+	public void demo1()
+	{
+		System.out.println("Demo 2 ");
+	}
+}
---
+++ src/test/java/com/Hidayath/DemoClass.java
@@ -0,0 +1,12 @@
+package com.Hidayath;
+
+import org.testng.annotations.Test;
+
+public class DemoClass 
+{
+	@Test
+	public void Demo()
+	{
+		System.out.println("Demo class");
+	}
+}
